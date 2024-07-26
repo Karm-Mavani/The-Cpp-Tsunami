c02247fb61fb13d1f414ef46eb5698639344ac4f
Karm-Mavani <karmmavani63@gmail.com>
2024-07-26 20:52:40 +0530

    26/7/2024

--- ch-3/1.cpp
+++ ch-3/1.cpp
@@ -1,29 +1,62 @@
 #include <iostream>
-#include<string.h>
+#include <string.h>
 using namespace std;
 
 class Student
 {
+public:
     int id;
     char name[20];
     int age;
     char course[20];
-    string city;
-    string email;
+    char city[20];
+    char email[40];
     char college[40];
 
 public:
-    void setdata(int i, char na[],char c,string ci,string e,char co)
-    { //
-        this->id = i;
-        strcpy(this->name,na);
+    void setdata(int i)
+    { 
+        cout << endl
+             << endl
+             << " For Student " << i + 1 << endl;
 
-        cout << na;
+        cout << "Enter ID: ";
+        cin >> id;
+
+        cout << "Enter Name: ";
+        cin >> name;
+        cout << "Enter Course :";
+        cin >> course;
+        cout << "Enter City :";
+        cin >> city;
+        cout << "Enter Email :";
+        cin >> email;
+        cout << "Enter College :";
+        cin >> college;
     }
+    void getdata(int i)
+    {
+        cout << endl
+             << endl
+             << "For Student" << i + 1 << endl
+             << endl;
+        cout << "Name :" << name << endl;
+        cout << "Age :" << age << endl;
+        cout << "Course :" << course << endl;
+        cout << "City :" << city << endl;
+        cout << "Email :" << email << endl;
+        cout << "College :" << college << endl;
+    }
+    
 };
 
 int main()
 {
-    Student s1;
-    //s1.setdata(15, "karm");
+    Student s1[4];
+   
+    for (int i = 0; i < 5; i++)
+        s1[i].setdata(i);
+    
+    for (int i = 0; i < 5; i++)
+        s1[i].getdata(i);
 }
